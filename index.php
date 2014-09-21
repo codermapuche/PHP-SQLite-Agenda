89a100684e17f6c54e9e2a02610ec7d4bbde62bd
Nehuen Prados <nehuensd@gmail.com>
2014-09-21 16:18:35 -0300

    V0.2
    Se agrego el css responsitive para la tabla y se agrego soporte para
    claves foraneas sqlite

--- index.php
+++ index.php
@@ -13,6 +13,207 @@
                 list.appendChild(nuevo)
             }
         </script>
+        <style>
+            body 
+            {
+                font-family: "Helvetica Neue", Helvetica, Arial;
+                font-size: 14px;
+                line-height: 20px;
+                font-weight: 400;
+                -webkit-font-smoothing: antialiased;
+                font-smoothing: antialiased;
+            }
+
+            /*/
+             * Datatatable Style
+            /*/
+            table.datatatable 
+            {
+                color: #333;
+                font-family: sans-serif;
+                font-size: .9em;
+                font-weight: 300;
+                text-align: left;
+                line-height: 35px;
+                border-spacing: 0;
+                border: 2px solid #428BCA;
+                width: 98%;
+                margin: 1%;
+                box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+                display: table;
+            }
+            
+            @media screen and (max-width: 580px) 
+            {
+                table.datatatable
+                {
+                    display: block;
+                }
+                
+                table.datatatable > tbody > tr
+                {
+                    padding: 8px 0;
+                    display: block;
+                }
+                
+                table.datatatable > tbody > tr:not(:last-child) 
+                {
+                    border-bottom: 2px solid #DDDDDD !important; 
+                }
+                
+                table.datatatable > tbody > tr > td, 
+                table.datatatable > tfoot > tr > th
+                {
+                    padding: 2px 12px;
+                    display: block;
+                }
+                
+                table.datatatable > tfoot > tr:first-child > th:not(:first-child):not(:last-child)
+                {
+                    border-bottom: 1px dotted #DDDDDD;                 
+                }
+                table.datatatable > tfoot > tr:first-child > th:not(:first-child)
+                {
+                    border-top: none;              
+                }
+                
+                table.datatatable > thead > tr:last-child
+                {
+                    display: none;
+                }
+                
+                table.datatatable > thead > tr > th.actions,
+                table.datatatable > tbody > tr > td.actions
+                {
+                    border-left: none !important; 
+                    width: 100% !important;
+                }
+            }
+            
+            table.datatatable > thead tr:first-child 
+            {
+                background: #428BCA;
+                color: #fff;
+                border: none;
+            }
+
+            table.datatatable > thead > tr > th 
+            {
+                font-weight: bold;
+                overflow: hidden;     
+            }
+            
+            table.datatatable > tbody > tr > td 
+            {                
+                padding-right: 10px;
+            }
+            
+            table.datatatable th:first-child, table.datatatable td:first-child 
+            {
+                padding: 0 10px 0 20px;
+            }
+
+            table.datatatable > thead > tr:last-child th 
+            {
+                border-bottom: 2px solid #DDDDDD; 
+                text-align: center;
+            }
+
+            table.datatatable > tbody > tr:not(:last-child) > td
+            {
+                border-bottom: 1px dotted #DDDDDD; 
+            }
+            
+            table.datatatable > tbody > tr 
+            {
+                display: table-row;
+                background: #FFFFFF;
+            }
+            
+            table.datatatable > tbody > tr:nth-of-type(odd) 
+            {
+                background: #FCFCFC;
+            }
+            
+            table.datatatable > tbody > tr:hover 
+            {
+                background-color: #F6F6F6;
+            }
+
+            table.datatatable > tfoot > tr:first-child th 
+            {
+                border-top: 2px solid #DDDDDD; 
+                text-align: center;
+            }
+            
+            table.datatatable > thead tr:first-child > th .button
+            {        
+                padding: 0 15px;
+                margin: 0 0 0 5px;
+                color: #333;
+                display: inline-block;
+                font-size: .9em;
+                line-height: 33px;
+                text-decoration: none;
+                float: right;
+                background: #F6F6F6;
+                text-transform: uppercase;
+            }
+            
+            table.datatatable > thead tr:first-child > th .button:not(.active) 
+            {               
+                background: rgba(100, 100, 100, .20);
+                color: #FFF;
+                border: none;
+            }
+            
+            table.datatatable > thead tr:first-child > th .button.active:hover
+            {               
+                color: #333;
+            }
+            
+            table.datatatable > thead tr:first-child > th .button:not(.active):hover 
+            {               
+                background: rgba(50, 50, 50, .20);
+            }
+            table.datatatable > thead > tr > th.actions,
+            table.datatatable > tbody > tr > td.actions,
+            table.datatatable > tfoot > tr > th.actions
+            {
+                border-left: 1px dotted #DDDDDD; 
+                width: 125px;
+                text-align: center;
+                text-transform: uppercase;
+            }
+            table.datatatable > tbody > tr > td .button,            
+            table.datatatable > tfoot > tr > th:last-child .button 
+            {
+                color: #333;
+                display: inline-block;
+                font-size: .9em;
+                padding: 0 5px;
+                line-height: 30px;
+                text-align: center;
+                text-decoration: none;
+                background: #F6F6F6;
+                text-transform: uppercase;
+                margin-right: -4px;
+            }
+            
+            table.datatatable > tbody > tr > td .button:not(:last-child), 
+            table.datatatable > tfoot > tr > th:last-child .button:not(:last-child)  
+            {
+                border-right: 1px solid #DDD;
+            }
+            
+            table.datatatable > tbody > tr > td .button:hover, 
+            table.datatatable > tfoot > tr > th:last-child .button:hover 
+            {
+                background: #EEE;
+                color: #428BCA;
+                cursor: pointer;
+            }
+        </style>
     </head>
     <body>
 <?php
@@ -20,39 +221,37 @@
 
     // Ubicacion de la base de datos:
     define("_FILE_DB_", "agenda.db");
-    $crear = !file_exists(_FILE_DB_);
+    define("_NEW_DB_", !file_exists(_FILE_DB_));
     $db = new SQLite3(_FILE_DB_);
 
     // En caso de que sea una base nueva, esto la crea.
-    if($crear)
+    if(_NEW_DB_)
     {
-        $db->exec("CREATE TABLE persona (id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, nombre STRING);");
-        $db->exec("CREATE TABLE telefono (id_persona INTEGER, telefono STRING);");
-        $db->exec("CREATE TABLE email (id_persona INTEGER, email STRING);");
+        $db->exec("CREATE TABLE persona 
+                    (
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+                        nombre STRING
+                    );");
+        $db->exec("CREATE TABLE telefono 
+                    (
+                        id_persona INTEGER NOT NULL REFERENCES persona(id) ON UPDATE CASCADE ON DELETE CASCADE, 
+                        telefono STRING
+                    );");
+        $db->exec("CREATE TABLE email 
+                    (
+                        id_persona INTEGER NOT NULL REFERENCES persona(id) ON UPDATE CASCADE ON DELETE CASCADE, 
+                        email STRING
+                    );");
     }
-
+    $db->exec("PRAGMA foreign_keys = ON;"); // Habilitar el uso de las claves foraneas que definimos.
+    
     // Hay que borrar la persona.
     if(isset($_GET["borrar"]))
     {
         // Eliminar la persona.
         $stmt = $db->prepare("DELETE FROM persona WHERE id = :id;"); // Preparar la sentencia.
         if(!$stmt) exit($db->lastErrorMsg()); // Morir en el intento si no puede prepararla.
-
         $stmt->bindValue(":id", $_GET["borrar"], SQLITE3_INTEGER); // Evitar problemas de inyeccion sql y de sintaxis.
-        $stmt->execute();
-
-        // Eliminar telefonos.
-        $stmt = $db->prepare("DELETE FROM telefono WHERE id_persona = :id_persona;");
-        if(!$stmt) exit($db->lastErrorMsg());
-
-        $stmt->bindValue(":id_persona", $_GET["borrar"], SQLITE3_INTEGER);
-        $stmt->execute();
-
-        // Eliminar emails.
-        $stmt = $db->prepare("DELETE FROM email WHERE id_persona = :id_persona;");
-        if(!$stmt) exit($db->lastErrorMsg());
-
-        $stmt->bindValue(":id_persona", $_GET["borrar"], SQLITE3_INTEGER);
         $stmt->execute();
     }
 
@@ -70,128 +269,118 @@
             // Esta editando.
             $stmt = $db->prepare("UPDATE persona SET nombre = :nombre WHERE id = :id;"); // Preparar la sentencia.
             if(!$stmt) exit($db->lastErrorMsg()); // Morir en el intento si no puede prepararla.
-
-            $stmt->bindValue(":id", $_POST["id"], SQLITE3_INTEGER); // Evitar problemas de inyeccion sql y de sintaxis.
             $stmt->bindValue(":nombre", $_POST["nombre"], SQLITE3_TEXT); // Evitar problemas de inyeccion sql y de sintaxis.
+            $stmt->bindValue(":id", $_POST["id"], SQLITE3_INTEGER); // Evitar problemas de inyeccion sql y de sintaxis.
             $stmt->execute();
 
             // Eliminar telefonos viejos.
             $stmt = $db->prepare("DELETE FROM telefono WHERE id_persona = :id_persona;");
-            if(!$stmt) exit($db->lastErrorMsg());
-
+            if(!$stmt) exit($db->lastErrorMsg());            
             $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
             $stmt->execute();
-
-            $_POST["tel"] = array_unique(array_filter($_POST["tel"])); // Eliminar telefonos vacios y duplicados.
-            if($_POST["tel"])
-            {
-                foreach($_POST["tel"] as $telefono)
-                {
-                    $stmt = $db->prepare("INSERT INTO telefono (id_persona, telefono) VALUES (:id_persona, :telefono);");
-                    if(!$stmt) exit($db->lastErrorMsg());
-
-                    $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
-                    $stmt->bindValue(":telefono", $telefono, SQLITE3_TEXT);
-                    $stmt->execute();
-                }
-            }
-
+            
             // Eliminar emails viejos.
             $stmt = $db->prepare("DELETE FROM email WHERE id_persona = :id_persona;");
-            if(!$stmt) exit($db->lastErrorMsg());
-
+            if(!$stmt) exit($db->lastErrorMsg());            
             $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
             $stmt->execute();
-
-            $_POST["email"] = array_unique(array_filter($_POST["email"])); // Eliminar emails vacios y duplicados.
-            if($_POST["email"])
-            {
-                foreach($_POST["email"] as $email)
-                {
-                    $stmt = $db->prepare("INSERT INTO email (id_persona, email) VALUES (:id_persona, :email);");
-                    if(!$stmt) exit($db->lastErrorMsg());
-
-                    $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
-                    $stmt->bindValue(":email", $email, SQLITE3_TEXT);
-                    $stmt->execute();
-                }
-            }
         }
         else
         {
             // Esta agregando.
             $stmt = $db->prepare("INSERT INTO persona (nombre) VALUES (:nombre);"); // Preparar la sentencia.
             if(!$stmt) exit($db->lastErrorMsg());
-
             $stmt->bindValue(":nombre", $_POST["nombre"], SQLITE3_TEXT); // Evitar problemas de inyeccion sql y de sintaxis.
             $stmt->execute();
             $_POST["id"] = $db->lastInsertRowID(); // Obtener el id de la persona.
-
-            $_POST["tel"] = array_unique(array_filter($_POST["tel"])); // Eliminar telefonos vacios y duplicados.
-            if($_POST["tel"])
+        }
+        
+        $_POST["tel"] = array_unique(array_filter($_POST["tel"])); // Eliminar telefonos vacios y duplicados.
+        if($_POST["tel"])
+        {
+            foreach($_POST["tel"] as $telefono)
             {
-                foreach($_POST["tel"] as $telefono)
-                {
-                    $stmt = $db->prepare("INSERT INTO telefono (id_persona, telefono) VALUES (:id_persona, :telefono);");
-                    if(!$stmt) exit($db->lastErrorMsg());
+                $stmt = $db->prepare("INSERT INTO telefono (id_persona, telefono) VALUES (:id_persona, :telefono);");
+                if(!$stmt) exit($db->lastErrorMsg());
 
-                    $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
-                    $stmt->bindValue(":telefono", $telefono, SQLITE3_TEXT);
-                    $stmt->execute();
-                }
+                $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
+                $stmt->bindValue(":telefono", $telefono, SQLITE3_TEXT);
+                $stmt->execute();
             }
+        }
 
-            $_POST["email"] = array_unique(array_filter($_POST["email"])); // Eliminar emails vacios y duplicados.
-            if($_POST["email"])
+        $_POST["email"] = array_unique(array_filter($_POST["email"])); // Eliminar emails vacios y duplicados.
+        if($_POST["email"])
+        {
+            foreach($_POST["email"] as $email)
             {
-                foreach($_POST["email"] as $email)
-                {
-                    $stmt = $db->prepare("INSERT INTO email (id_persona, email) VALUES (:id_persona, :email);");
-                    if(!$stmt) exit($db->lastErrorMsg());
+                $stmt = $db->prepare("INSERT INTO email (id_persona, email) VALUES (:id_persona, :email);");
+                if(!$stmt) exit($db->lastErrorMsg());
 
-                    $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
-                    $stmt->bindValue(":email", $email, SQLITE3_TEXT);
-                    $stmt->execute();
-                }
+                $stmt->bindValue(":id_persona", $_POST["id"], SQLITE3_INTEGER);
+                $stmt->bindValue(":email", $email, SQLITE3_TEXT);
+                $stmt->execute();
             }
         }
     }
 
-    // Traer a todas las personas
+    // Cuenta a las personas
     $stmt = $db->prepare("SELECT COUNT(*) AS cantidad FROM persona;");
     if(!$stmt) exit($db->lastErrorMsg());
+    $cantidad = $stmt->execute()->fetchArray(SQLITE3_ASSOC)["cantidad"];
 
-    $result = $stmt->execute();
-    $cantidad = $result->fetchArray(SQLITE3_ASSOC)["cantidad"];
-
-    $stmt = $db->prepare("SELECT id, nombre FROM persona;");
+    // Traer a todas las personas
+    $stmt = $db->prepare("SELECT 
+                                persona.id, 
+                                persona.nombre, 
+                                GROUP_CONCAT(telefonos.telefonos, ', ') telefonos, 
+                                GROUP_CONCAT(emails.emails, ', ') emails
+                            FROM persona 
+                                LEFT JOIN   ( SELECT 
+                                                    telefono.id_persona id_persona, 
+                                                    GROUP_CONCAT(telefono.telefono, ', ') telefonos 
+                                                FROM telefono
+                                                 GROUP BY telefono.id_persona
+                                            ) telefonos
+                                LEFT JOIN   ( SELECT 
+                                                    email.id_persona id_persona, 
+                                                    GROUP_CONCAT(email.email, ', ') emails 
+                                                FROM email
+                                                 GROUP BY email.id_persona
+                                            ) emails
+                                    ON emails.id_persona = persona.id
+                            GROUP BY persona.id;");
     if(!$stmt) exit($db->lastErrorMsg());
     $result = $stmt->execute();
 ?>
-    <table>
+    <table class="datatatable">
         <thead>
             <tr>
-                <th colspan="2">
-                    AGENDA
-                </th>
-                <th>
-                    <a href="index.php">Agregar</a>
+                <th colspan="5">
+                    AGENDA SQLITE
+                    <a href="index.php" class="button">Agregar</a>
+                    <a href="index.php" class="button active">Listado</a>
                 </th>
             </tr>
             <tr>
                 <th>
-                    Codigo
+                    #
                 </th>
                 <th>
                     Nombre
                 </th>
                 <th>
+                    Telefonos
+                </th>
+                <th>
+                    Emails
+                </th>
+                <th class="actions">
                     Acciones
                 </th>
             </tr>
         </thead>
         <tbody>
-        </tbody>
         <?php
             if($cantidad)
             {
@@ -199,15 +388,22 @@
                 {
         ?>
             <tr>
-                <th>
+                <td>
                     <?=$persona["id"];?>
-                </th>
-                <th>
+                </td>
+                <td>
                     <?=$persona["nombre"];?>
-                </th>
-                <th>
-                    <a href="index.php?editar=<?=$persona["id"];?>">Editar</a> / <a href="index.php?borrar=<?=$persona["id"];?>" onclick="return confirm('Esta seguro de eliminar el registro?');"> Borrar </a>
-                </th>
+                </td>
+                <td>
+                    <?=$persona["telefonos"];?>
+                </td>
+                <td>
+                    <?=$persona["emails"];?>
+                </td>
+                <td class="actions">
+                    <a href="index.php?borrar=<?=$persona["id"];?>" class="button" onclick="return confirm('Esta seguro de eliminar el registro?');"> Borrar </a>
+                    <a href="index.php?editar=<?=$persona["id"];?>" class="button">Editar</a>
+                </td>
             </tr>
 
         <?php
@@ -217,7 +413,7 @@
             {
         ?>
             <tr>
-                <td colspan="3">
+                <td colspan="5">
                     No se encontraron resultados.
                 </td>
             </tr>
@@ -227,8 +423,14 @@
         </tbody>
         <tfoot>
             <tr>
-                <th colspan="3">
+                <th colspan="2">
                    <?=$cantidad;?> Personas
+                </th>
+                <th colspan="2">
+                </th>
+                <th class="actions">                    
+                    <a href="#" class="button"><</a>
+                    <a href="#" class="button">></a>
                 </th>
             </tr>
         </tfoot>
